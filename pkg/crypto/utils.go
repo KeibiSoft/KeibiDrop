0e7aebdf435dc4dffa4dbf1180074324345d89db
Marius-Florin Cristian <marius@keibisoft.com>
2025-05-09 15:43:37 +0300

    WIP: Fix lints

--- pkg/crypto/utils.go
+++ pkg/crypto/utils.go
@@ -1,6 +1,9 @@
 package crypto
 
-import "crypto/rand"
+import (
+	"crypto/rand"
+	"fmt"
+)
 
 func RandomBytes(size int) ([]byte, error) {
 	b := make([]byte, size)
@@ -15,4 +18,11 @@ func RandomBytes(size int) ([]byte, error) {
 func GenerateSeed() []byte {
 	res, _ := RandomBytes(64)
 	return res
+}
+
+func safeInt(u uint64) (int, error) {
+	if u > uint64(^uint(0)>>1) {
+		return 0, fmt.Errorf("value too large to fit into int: %d", u)
+	}
+	return int(u), nil
 }
