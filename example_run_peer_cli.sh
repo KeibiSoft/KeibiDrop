a6396f3a5f1897cabe8c4d7b1be52132e9566a50
Marius-Florin Cristian <marius@keibisoft.com>
2025-10-18 09:48:39 +0300

    Check

--- example_run_peer_cli.sh
+++ example_run_peer_cli.sh
@@ -1,2 +1,2 @@
-TO_SAVE_PATH="/Users/marius/work/code/KeibiDrop/SaveBob" TO_MOUNT_PATH="/Users/marius/work/code/KeibiDrop/MountBob" KEIBIDROP_RELAY="http://0.0.0.0:54321" INBOUND_PORT=26003 OUTBOUND_PORT=26004 go run cmd/cli/keibidrop-cli.go
+TO_SAVE_PATH="/Users/marius/work/code/KeibiDrop/SaveBob" TO_MOUNT_PATH="/Users/marius/work/code/KeibiDrop/MountBob" KEIBIDROP_RELAY="https://keibidroprelay.keibisoft.com" INBOUND_PORT=26003 OUTBOUND_PORT=26004 go run cmd/cli/keibidrop-cli.go
 
